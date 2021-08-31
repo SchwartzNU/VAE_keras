db187aa3e469f70cc828cbf8f5cb2a9e7f909f50
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-08-30 23:35:18 -0500

    added generation part

--- GenerateFromTrainedModel.py
+++ GenerateFromTrainedModel.py
@@ -8,7 +8,7 @@ Created on Mon Aug 30 14:48:39 2021
 import os
 import numpy as np
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 def load_dataset_with_labels(dataset_dir):
     train_set = tf.keras.preprocessing.image_dataset_from_directory(
@@ -42,15 +42,33 @@ def get_label_list(dataset):
         L.append(dataset.class_names[temp[0,0]])
     return L
 
-
-def generate_data(dataset, N_per_type=10):
+def generate_data(model, dataset, N_per_type=5):
     types = get_label_list(dataset)
     unique_types = list(set(types))
+    dataset_unbatched = dataset.unbatch()
+    plt.ioff()
     for i in range(len(unique_types)):
-        ind = [index for index, element in enumerate(types) if element == unique_types[i]]
-        os.makedir(unique_types[i],exist_ok=True)
-        for j in range(N_per_type):
-            
-        print(ind)
-        
+        print(unique_types[i])
+        #ind = [index for index, element in enumerate(types) if element == unique_types[i]]
+        os.makedirs(os.path.join('generated',unique_types[i]),exist_ok=True)
+        gen_counter = 0
+        while gen_counter < N_per_type:
+            for d, label in dataset_unbatched.as_numpy_iterator():
+                temp = np.argwhere(np.array(label > 0))
+                cur_type = dataset.class_names[temp[0,0]]
+                if cur_type == unique_types[i]:
+                    _, _, z_sample = model.encoder.predict(d[None,:,:,:])
+                    new_data = model.decoder.predict(z_sample)
+                    # breakpoint()
+                    plt.figure(figsize=(8, 2),
+                               tight_layout=True,
+                               dpi=300,
+                               frameon=False)
+                    plt.imshow(new_data[0, :, :, 0], cmap='gray')
+                    plt.axis('off')
+                    fname = os.path.join('generated',unique_types[i],'{:4d}.jpg'.format(gen_counter))
+                    plt.savefig(fname)
+                    plt.close()
+                    gen_counter = gen_counter+1
+                    
 #z_mean, _, _ = vae.encoder.predict(train_set)
