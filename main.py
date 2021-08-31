518cf535269958b92bf9869648306db2e55172f0
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-08-30 21:12:12 -0500

    more adam params

--- main.py
+++ main.py
@@ -27,9 +27,19 @@ parser.add_argument("-epochs",
 parser.add_argument("-loadweights", 
                     type=str, 
                     help="weight file to start")
+parser.add_argument("-adam_alpha", 
+                    type=float, 
+                    default=0.001,
+                    help="beta_1 parameter for the Adam optimizer algorithm")
+parser.add_argument("-adam_beta1", 
+                    type=float, 
+                    default=0.9,
+                    help="beta_1 parameter for the Adam optimizer algorithm")
 parser.add_argument("-adam_beta2", 
                     type=float, 
+                    default=0.999,
                     help="beta_2 parameter for the Adam optimizer algorithm")
+
 args = parser.parse_args()
 
 latent_dim = args.latent_dim
@@ -41,11 +51,6 @@ if args.loadweights is not None:
 else:
     weights_fname = None
     
-if args.adam_beta2 is not None:
-    beta2 = args.adam_beta2
-else:
-    beta2 = None    
-
 class Sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
 
@@ -144,10 +149,9 @@ for test_batch in test_set.take(1):
 
 # %%make model
 vae = VAE(encoder, decoder)
-if beta2 is not None:
-    optimizer = keras.optimizers.Adam(beta_2 = beta2)
-else:
-    optimizer = keras.optimizers.Adam()
+optimizer = keras.optimizers.Adam(learning_rate = args.adam_alpha,
+                                  beta_1 = args.adam_beta1,
+                                  beta_2 = args.adam_beta2)
 
 vae.compile(optimizer=optimizer)
 
